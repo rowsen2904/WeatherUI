4a9f6a3b5953b49f7b41d68facfa700fdcf23ec3
Rowsen <bayramovw0@gmail.com>
2023-09-07 18:22:39 +0500

    Initial Commit

---
+++ app/appApp.swift
@@ -0,0 +1,17 @@
+//
+//  appApp.swift
+//  app
+//
+//  Created by Rowsen on 9/7/23.
+//
+
+import SwiftUI
+
+@main
+struct appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
